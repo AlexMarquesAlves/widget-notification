e3d1e9ad7cc45272bef4a28f0a59115a64acce07
¡Duque <atanaelleonardo@gmail.com>
2024-04-27 16:10:40 -0300

    Change background color of main element in home page
    Add recent section header in widget component
    
    Change background color of recent section header in widget component
    
    These changes are based on the provided code diff. The first commit message describes the modification of

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Widget } from '@/components'
 
 export default function Home() {
   return (
-    <main className="h-screen bg-zinc-900 flex items-center justify-center text-zinc-50">
+    <main className="h-screen bg-zinc-950 flex items-center justify-center text-zinc-50">
       <Widget></Widget>
     </main>
   )
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -8,12 +8,21 @@ export function Widget({ children }: WidgetProps) {
   return (
     <>
       <div className="w-[448px] rounded overflow-hidden">
+        {/* Header */}
         <div className="bg-zinc-800 py-4 px-6 flex items-center justify-between">
           <span className="font-bold">Notificações</span>
           <button className="text-violet-500 font-bold text-xs hover:text-violet-400">
             MARCAR TODAS COMO VISTAS
           </button>
         </div>
+
+        {/* Recent Section */}
+        <div className="">
+          <div className="bg-zinc-900 px-5 py-2 text-sm text-zinc-400">
+            Recentes
+          </div>
+        </div>
+        {/* Ancient Section */}
       </div>
       {children}
     </>
