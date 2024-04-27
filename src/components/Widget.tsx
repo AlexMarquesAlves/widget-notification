c7535e01d2ebd6b3b78e9afacc8870ceb32f7ec1
¡Duque <atanaelleonardo@gmail.com>
2024-04-27 16:05:25 -0300

    Change HTML language to Portuguese (Brazil) and add "MARK ALL AS READ" button to Widget component
    This commit changes the HTML language attribute from "en" to "pt-BR" in the RootLayout component and adds a new

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -10,6 +10,9 @@ export function Widget({ children }: WidgetProps) {
       <div className="w-[448px] rounded overflow-hidden">
         <div className="bg-zinc-800 py-4 px-6 flex items-center justify-between">
           <span className="font-bold">Notificações</span>
+          <button className="text-violet-500 font-bold text-xs hover:text-violet-400">
+            MARCAR TODAS COMO VISTAS
+          </button>
         </div>
       </div>
       {children}
