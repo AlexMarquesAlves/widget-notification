835794b5fd2752bcf33eccfe2076a8aedf1f277b
¡Duque <atanaelleonardo@gmail.com>
2024-04-28 14:50:12 -0300

    feat: "Add recent notifications section to Widget component"
    This commit adds a new section to the Widget component that displays recent notifications. The notifications are displayed in a list with a gray background and a divider between each item. Each notification

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -55,6 +55,42 @@ export function Widget({ children }: WidgetProps) {
           </div>
         </div>
         {/* Ancient Section */}
+        <div className="">
+          <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+            Recentes
+          </div>
+
+          <div className="divide-y-2 divide-zinc-950">
+            {/* notification block */}
+            <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+              <Rocket className="h-6 w-6 text-violet-500 mt-3" />
+              <div className="flex-1 flex flex-col gap-2">
+                <p className="text-sm leading-relaxed text-zinc-100">
+                  Voce recebeu um convite para fazer parte da empresa
+                  Rocketseat.
+                </p>
+                <div className="text-xxs text-zinc-400 flex items-center gap-x-1">
+                  <span>Convite</span>
+                  <span>Há 3 min</span>
+                </div>
+              </div>
+            </div>
+            {/* notification block */}
+            <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+              <Rocket className="h-6 w-6 text-violet-500 mt-3" />
+              <div className="flex-1 flex flex-col gap-2">
+                <p className="text-sm leading-relaxed text-zinc-100">
+                  Voce recebeu um convite para fazer parte da empresa
+                  Rocketseat.
+                </p>
+                <div className="text-xxs text-zinc-400 flex items-center gap-x-1">
+                  <span>Convite</span>
+                  <span>Há 3 min</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       {children}
     </>
