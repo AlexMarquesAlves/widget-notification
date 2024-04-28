2e79dc8d8cd21c8d128c1af605bd4eecf5f9fe59
¡Duque <atanaelleonardo@gmail.com>
2024-04-28 18:43:23 -0300

    test: "Adds specs for rendering header component in widget-notification"
    This commit adds new specs for testing the rendering of the header component in the 'widget-notification' feature. It includes checks for the visibility of the header, the

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -6,4 +6,22 @@ describe('widget-notification spec', () => {
   it('should render page with title "Widget Notification"', () => {
     cy.get('title').should('contain', 'Widget Notification')
   })
+
+  describe('should render header component successfully', () => {
+    it('should render header successfully', () => {
+      cy.get('div.px-6').should('be.visible')
+    })
+
+    it('should render header title successfully', () => {
+      cy.get('div>span.font-bold')
+        .should('be.visible')
+        .should('contain', 'Notificações')
+    })
+
+    it('should render header button successfully', () => {
+      cy.get('div > button.text-violet-500')
+        .should('be.visible')
+        .should('contain', 'MARCAR TODAS COMO VISTAS')
+    })
+  })
 })
