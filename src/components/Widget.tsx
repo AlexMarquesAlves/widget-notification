ab4814ff7aa9f8ad79e54ea147714a00cddb145e
¡Duque <atanaelleonardo@gmail.com>
2024-04-28 15:55:45 -0300

    style: Update Widget button styles with dark mode support
    * Change button background and hover colors for the close button
    * Add dark mode support for the close button using classnames
    * Update button background and hover colors for the action button

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -53,7 +53,7 @@ export function Widget({ children }: WidgetProps) {
               </div>
 
               <div className="flex gap-2 self-center">
-                <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-800 hover:bg-zinc-700">
+                <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500  dark:bg-zinc-800 dark:hover:bg-zinc-700">
                   <X className="w-3 h-3 text-zinc-50" />
                 </button>
                 <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
