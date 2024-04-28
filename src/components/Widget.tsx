89860bf407f84a9ade74288d58b13a81d12b82a5
¡Duque <atanaelleonardo@gmail.com>
2024-04-28 15:51:58 -0300

    style: Change Widget background and text color to support dark mode
    * Change header background to zinc-200 (dark:zinc-800)
    * Change recent section background to zinc-300 (dark:z

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -10,7 +10,7 @@ export function Widget({ children }: WidgetProps) {
     <>
       <div className="w-[448px] rounded overflow-hidden">
         {/* Header */}
-        <div className="bg-zinc-800 py-4 px-6 flex items-center justify-between">
+        <div className="bg-zinc-200 dark:bg-zinc-800 py-4 px-6 flex items-center justify-between">
           <span className="font-bold">Notificações</span>
           <button className="text-violet-500 font-bold text-xs hover:text-violet-400">
             MARCAR TODAS COMO VISTAS
@@ -19,16 +19,16 @@ export function Widget({ children }: WidgetProps) {
 
         {/* Recent Section */}
         <div>
-          <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+          <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-400">
             Recentes
           </div>
 
-          <div className="divide-y-2 divide-zinc-950">
+          <div className="divide-y-2 divide-zinc-500 dark:divide-zinc-950">
             {/* notification block */}
-            <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+            <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
               <Rocket className="h-6 w-6 text-violet-500 mt-3" />
               <div className="flex-1 flex flex-col gap-2">
-                <p className="text-sm leading-relaxed text-zinc-100">
+                <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
                   Voce recebeu um convite para fazer parte da empresa
                   Rocketseat.
                 </p>
